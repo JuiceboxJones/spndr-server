c7e5024cfb6cbfa03695ee6529f4e5393ad86b71
Juicebox Jones <jderhalli@gmail.com>
2019-03-26 12:16:00 -0700

    changed database structure

--- migrations/003.do.create_spndr_expenses.sql
+++ migrations/003.do.create_spndr_expenses.sql
@@ -2,7 +2,7 @@ CREATE TABLE spndr_expenses (
   id serial primary key,
   name text not null,
   amount numeric not null,
-  date_created timestamp not null default now()
+  date_created timestamp not null default now(),
   user_id integer 
    references spndr_users(id) on delete cascade not null
 );
